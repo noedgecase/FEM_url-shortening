1d10adcba4beb9a83221d2f799339eead9b2ebef
dragos <dragos042@yahoo.com>
2021-05-16 19:19:40 +0300

    progress with error messages

--- src/components/Sections/URLInputBox.js
+++ src/components/Sections/URLInputBox.js
@@ -2,12 +2,34 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import formMobileBackground from '../../images/bg-shorten-mobile.svg'
 import formDesktopBackground from '../../images/bg-shorten-desktop.svg'
+import sprite from '../../sprite.svg'
 
 const StyledContainer = styled.section`
+  position: relative;
   width: 100%;
   height: auto;
   position: relative;
   bottom: 75px;
+
+  .visible {
+    opacity: 1 !important;
+    transition: opacity 350ms ease-in-out, min-height 300ms ease-in-out,
+      max-height 300ms ease-in-out;
+    max-height: 28px !important;
+    min-height: 28px !important;
+  }
+  .red-border {
+    input[type='text'] {
+      border: 3px solid hsl(0, 87%, 67%);
+    }
+  }
+  .unclickable {
+    pointer-events: none;
+  }
+  .unclickable-button {
+    background-color: hsl(180, 72%, 80%);
+    pointer-events: none;
+  }
 `
 
 const StyledInputBox = styled.div`
@@ -16,7 +38,7 @@ const StyledInputBox = styled.div`
   flex-direction: column;
   align-items: center;
   margin: 0 auto;
-  padding: 1em;
+  padding: 0.5em 1.5em;
   width: clamp(5em, 90%, 1400px);
   background-color: hsl(257, 27%, 26%);
   background-image: url(${formMobileBackground});
@@ -34,10 +56,12 @@ const StyledInputBox = styled.div`
 `
 
 const StyledURLInputForm = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
   width: 100%;
+  padding: 1em 0;
   input[type='text'] {
     width: 100%;
     font-family: 'Poppins', sans-serif;
@@ -49,16 +73,22 @@ const StyledURLInputForm = styled.div`
     padding: 0.8em;
     border: none;
     overflow: hidden;
+    border: 3px solid transparent;
+    transition: border 200ms;
     ::placeholder {
       color: hsl(257, 7%, 63%);
     }
   }
   button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     font-family: 'Poppins', sans-serif;
     font-size: 1rem;
     font-weight: 700;
     color: white;
     width: 100%;
+    min-height: 50px;
     border: none;
     background-color: hsl(180, 66%, 49%);
     padding: 0.8em;
@@ -72,7 +102,31 @@ const StyledURLInputForm = styled.div`
       transition: background-color 300ms;
     }
   }
+  #error-message {
+    color: hsl(0, 87%, 67%);
+    font-weight: 500;
+    font-style: italic;
+    max-height: 0px;
+    min-height: 0px;
+    opacity: 0;
+    transition: opacity 350ms ease-in-out, min-height 300ms ease-in-out,
+      max-height 300ms ease-in-out;
+    user-select: none;
+  }
+  .loading-animation {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    fill: white;
+    animation: loading 1500ms linear infinite;
+  }
+  @keyframes loading {
+    to {
+      transform: rotate(360deg);
+    }
+  }
   @media screen and (min-width: 780px) {
+    padding: 0;
     flex-direction: row;
     input[type='text'] {
       margin: 0 0.5em 0 0;
@@ -86,59 +140,103 @@ const StyledURLInputForm = styled.div`
         transition: transform 200ms;
       }
     }
+    #error-message {
+      position: absolute;
+      bottom: -32px;
+      transition: opacity 350ms ease-in-out, min-height 300ms ease-in-out,
+        max-height 300ms ease-in-out;
+      min-height: 25px;
+      max-height: 25px;
+    }
   }
 `
 
 const StyledGeneratedLink = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
   border-radius: 0.4em;
   text-align: center;
-  width: clamp(5em, 100%, 40em);
+  width: clamp(5em, 90%, 1400px);
   margin: 1em auto 0.5em auto;
-  height: 4em;
-  padding: 0 0 0 2em;
   background-color: #fff;
+  padding: 0 0 0.5em 0;
+  h3 {
+    font-size: 0.8rem;
+    font-weight: 500;
+  }
+  button {
+    font-family: 'Poppins', sans-serif;
+    font-size: 0.8rem;
+    font-weight: 700;
+    color: white;
+    border: none;
+    background-color: hsl(180, 66%, 49%);
+    padding: 0.4em 2em;
+    margin: 0.5em;
+    border-radius: 0.5em;
+    transition: background-color 300ms, transform 200ms;
+    width: clamp(8em, 80%, 25em);
+    height: 3em;
+    &:hover {
+      cursor: pointer;
+      background-color: hsl(180, 72%, 80%);
+      transition: background-color 300ms;
+    }
+  }
   .link-button {
     flex: 1;
     display: flex;
     justify-content: space-between;
     align-items: center;
-  }
-  h3 {
-    font-size: 0.8rem;
-    font-weight: 500;
+    flex-direction: column;
+    width: 100%;
   }
   .initial-link {
+    text-align: center;
+    margin: 1em auto;
     flex: 2;
+    padding: 0 1em;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
-    text-align: left;
-    width: clamp(10ch, 100%, 40ch);
+    width: clamp(10ch, 100%, 80ch);
     color: hsl(257, 27%, 26%);
   }
   .new-link {
     color: hsl(180, 66%, 49%);
-    text-align: center;
+    text-align: left;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin: 1em;
   }
-  button {
-    font-family: 'Poppins', sans-serif;
-    font-size: 0.8rem;
-    font-weight: 500;
-    color: white;
-    border: none;
-    background-color: hsl(180, 66%, 49%);
-    padding: 0.4em 2em;
-    margin: 0 1em;
-    border-radius: 0.2em;
-    transition: background-color 300ms, transform 200ms;
-    pointer-events: all;
-    &:hover {
-      cursor: pointer;
-      background-color: hsl(180, 72%, 80%);
-      transition: background-color 300ms;
+  .separator {
+    width: 100%;
+    height: 2px;
+    background-color: hsla(0, 0%, 26.666666666666668%, 0.103);
+  }
+  .copied {
+    background-color: hsl(257, 27%, 26%);
+    user-select: none;
+    pointer-events: none;
+  }
+  @media screen and (min-width: 780px) {
+    width: clamp(730px, 70%, 90em);
+    flex-direction: row;
+    padding: 1em;
+    button {
+      margin: 0 1em;
+      width: clamp(8em, 90%, 10em);
+    }
+    .separator {
+      display: none;
+    }
+    .link-button {
+      flex-direction: row;
+    }
+    .initial-link {
+      text-align: left;
     }
   }
 `
@@ -147,33 +245,72 @@ const URLInputForm = () => {
   let [linkInput, setLinkInput] = useState('')
   let [newLink, setNewLink] = useState('')
   const [longLink, setLongLink] = useState('')
+  const [emptyInput, setEmptyInput] = useState(false)
+  const [fetching, setFetching] = useState(false)
+  const [copied, setCopied] = useState(false)
 
   const updateInput = (e) => {
     setLinkInput(e.target.value)
   }
 
+  // trebuie timeout
+  // const emptyInputAction = () =>{
+  //   setEmptyInput(true)
+  // }
+
+  const copyClick = () => {
+    setCopied(true)
+    setTimeout(function () {
+      setCopied(false)
+    }, 3000)
+  }
+
+  const emptyInputShort = () => {
+    setEmptyInput(true)
+    setTimeout(function () {
+      setEmptyInput(false)
+    }, 3000)
+  }
+
+  const errorCheck = (error) => {
+    console.log(error)
+  }
+
   ////////// API fetch //////////
-  ///////////////////////////////
   const shortenLink = async () => {
-    // add generatedLink component
-    try {
-      const response = await fetch(
-        `https://api.shrtco.de/v2/shorten?url=${linkInput}`
-      )
-      const data = await response.json()
-      newLink = setNewLink(data.result?.short_link2 || data.error)
-      // remove loading animation
-    } catch (error) {
-      console.error(error)
+    if (linkInput.length === 0) {
+      emptyInputShort()
+    } else {
+      setFetching(true)
+      setEmptyInput(false)
+      try {
+        const response = await fetch(
+          `https://api.shrtco.de/v2/shorten?url=${linkInput}`,
+          {
+            method: 'GET',
+            credentials: 'same-origin',
+          }
+        )
+        const data = await response.json()
+        data.result
+          ? setNewLink(data.result.full_short_link2)
+          : errorCheck(data.error_code)
+        // newLink = setNewLink(
+        //   data.result?.full_short_link2 || errorCheck(data.error_code)
+        // )
+      } catch (error) {
+        console.error(error)
+      }
+      setLongLink(linkInput)
+      setLinkInput('')
+      setFetching(false)
     }
-    setLongLink(linkInput)
-    setLinkInput('')
   }
   ////////////////////////////////
 
   return (
     <StyledContainer>
-      <StyledInputBox>
+      <StyledInputBox className={emptyInput ? 'red-border' : null}>
         <StyledURLInputForm>
           <input
             type='text'
@@ -182,21 +319,37 @@ const URLInputForm = () => {
             onChange={updateInput}
             value={linkInput}
             placeholder='Shorten a link here...'
+            className={fetching ? 'unclickable' : null}
           />
-          <button onClick={shortenLink} id='shorten-btn'>
-            Shorten it!
+          <h4 id='error-message' className={emptyInput ? 'visible' : null}>
+            Please add a link
+          </h4>
+          <button
+            onClick={shortenLink}
+            className={fetching ? 'unclickable-button' : null}
+            id='shorten-btn'>
+            {fetching ? (
+              <svg className='loading-animation'>
+                <use href={sprite + '#cog'} />
+              </svg>
+            ) : (
+              'Shorten it!'
+            )}
           </button>
         </StyledURLInputForm>
       </StyledInputBox>
-      {/* asta de jos trebuie sa se construiasca cu fiecare link nou generat */}
-      <StyledGeneratedLink className='hidden'>
+      <StyledGeneratedLink className=''>
         <h3 className='initial-link'>{longLink}</h3>
+        <div className='separator'></div>
         <div className='link-button'>
-          <h3 className='new-link'>{newLink}</h3>
-          <button id='copy-btn'>Copy</button>
+          <h3 className='new-link' id='generated-link'>
+            {newLink}
+          </h3>
+          <button onClick={copyClick} className={copied ? 'copied' : null}>
+            {copied ? 'Copied!' : 'Copy'}
+          </button>
         </div>
       </StyledGeneratedLink>
-      {/*  */}
     </StyledContainer>
   )
 }
